7e8852acee6661a3d651a77b77a31d97fdb055ed
Vukasin <vule993@outlook.com>
2021-10-18 21:14:21 +0200

    init
    1. .NET Core project
    2. Create Product class
    3. Create Supplier class
    4. JSON file created
    5. Displaying data

---
+++ Products/Products/Program.cs
@@ -0,0 +1,22 @@
+﻿using Products.Common;
+using System;
+using System.Threading.Tasks;
+
+namespace Products
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Megastore megastore = new Megastore("../CentralStorage.json");
+
+            foreach(var supplier in await megastore.LoadAllSuppliers())
+            {
+                Console.WriteLine(supplier);
+            }
+
+            Console.WriteLine("Press any key to close the applicaion.");
+            Console.ReadKey();
+        }
+    }
+}
